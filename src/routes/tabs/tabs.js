89dfdc7816c52ddf6e67a8a82f4bfa3b66ccc7a5
Ishan Gogna <ishangogna10@gmail.com>
2020-07-29 12:03:36 +0530

    added theme settings

--- src/routes/tabs/tabs.js
+++ src/routes/tabs/tabs.js
@@ -2,15 +2,46 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native'
 import MainPage from '../../screens/main';
 import SettingsPage from '../../screens/settings';
-import React from 'react';
+import React, { useContext } from 'react';
+import { Entypo } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
+import { ThemeContext } from '../../contexts/ThemeContext';
+
+
 const tab = createBottomTabNavigator();
 
 const Tabs = () => {
+    const { theme } = useContext(ThemeContext);
     return ( 
         <NavigationContainer>
-            <tab.Navigator>
-                <tab.Screen name = 'Home' component = {MainPage}/>
-                <tab.Screen name = 'Settings' component = {SettingsPage}/>
+            <tab.Navigator
+                tabBarOptions={{
+                activeTintColor: '#FF3B53',
+                style : {
+                    backgroundColor : theme.bg,
+                    borderTopColor : theme.bg
+                }
+            }}
+            
+            >
+                <tab.Screen 
+                    name = 'Home' 
+                    component = {MainPage}
+                    options = {{
+                        tabBarIcon: ({ color, size }) => (
+                            <Entypo name="home" size={size} color={color} />
+                        ),
+                    }}  
+                    />
+                <tab.Screen 
+                    name = 'Settings' 
+                    component = {SettingsPage}
+                    options = {{
+                        tabBarIcon: ({ color, size }) => (
+                            <Feather name="settings" size={size} color={color} />
+                        ),
+                    }}  
+                    />
             </tab.Navigator>
         </NavigationContainer>
      );
--- src/screens/settings.js
+++ src/screens/settings.js
@@ -1,17 +1,69 @@
 import React, { useContext } from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
+import { View, StyleSheet, Text, Dimensions, TouchableOpacity } from 'react-native';
 import { ThemeContext } from '../contexts/ThemeContext';
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 const SettingsPage = () => {
     const { theme, dispatch } = useContext(ThemeContext);
-
     return ( 
-        <View style = {{flex : 1, backgroundColor : theme.bg}}>
-            <Text style = {{color : theme.syntax}}>Settings Page</Text>
-            <Button title = 'Dark Mode' onPress = {()=>dispatch({type:'DARK_MODE'})}></Button>
-            <Button title = 'Light Mode' onPress = {()=>dispatch({type: 'LIGHT_MODE'})}></Button>
+        <View style = {[styles.settingsContainer,{ backgroundColor : theme.bg}]}>
+            <Text style = {[styles.header, {color : theme.syntax}]}>Settings Page</Text>
+            <View style = {styles.settings}>
+                <Text style = {styles.version}>v1.0.0</Text>
+                <Text style = {[styles.subHeading,{color : theme.syntax}]}>Change themes</Text>
+                <View style = {styles.options}>
+                    <TouchableOpacity onPress = {()=>dispatch({type:'DARK_MODE'})}>
+                        <View style = {styles.themeContainer}>
+                            <Text style = {styles.theme}>Dark</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress = {()=>dispatch({type:'LIGHT_MODE'})}>
+                        <View style = {styles.themeContainer}>
+                            <Text style = {styles.theme}>Light</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            </View>
         </View>
      );
 }
- 
+const styles = StyleSheet.create({
+    settingsContainer : {
+        flex : 1,
+        paddingHorizontal : 20,
+    },
+    header : {
+        marginTop : windowHeight/15,
+        fontSize : 32,
+        fontWeight : 'bold'
+    },
+    version : {
+        color : 'grey',
+    },
+    options : {
+        marginTop : 15,
+        flexDirection : 'row',
+        justifyContent : 'flex-start'
+    },
+    themeContainer : {
+        height : windowHeight/15,
+        width : windowWidth/5,
+        backgroundColor : '#1DA1F2',
+        borderRadius : windowHeight/100,
+        justifyContent : 'center',
+        alignItems : 'center',
+        marginRight : 10,
+    },
+    theme : {
+        color: 'white',
+    
+    },
+    subHeading : {
+        fontSize : 20,
+        marginTop : 30,
+    }
+})
+
 export default SettingsPage;
